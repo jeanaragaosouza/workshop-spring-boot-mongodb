f13d60feb5152e0fb23594712a4ff2c241885085
jeanaragaosouza <tuning.jean@hotmail.com>
2025-11-28 15:51:30 -0300

    Entity User e Rest

---
+++ src/main/java/com/jeanaragao/workshopmongo/resources/UserResource.java
@@ -0,0 +1,33 @@
+package com.jeanaragao.workshopmongo.resources;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jeanaragao.workshopmongo.domain.User;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@RequestMapping(method = RequestMethod.GET) // ou poderia ser @GetMapping
+	public ResponseEntity<List<User>> findAll() {
+		User maria = new User("1", "Maria", "maria@gmail.com");
+		User alex = new User("2", "Alex", "maria@gmail.com");
+
+		List<User> list = new ArrayList<>();
+
+		list.addAll(Arrays.asList(maria, alex));
+
+		return ResponseEntity.ok().body(list);
+
+	}
+
+}
