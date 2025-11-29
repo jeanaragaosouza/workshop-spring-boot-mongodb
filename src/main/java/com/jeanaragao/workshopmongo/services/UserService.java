235099ac28657a76c8e0e01d64a08b22ac9f0e22
jeanaragaosouza <tuning.jean@hotmail.com>
2025-11-29 14:10:31 -0300

    Insercao de usuario com POST

--- src/main/java/com/jeanaragao/workshopmongo/services/UserService.java
+++ src/main/java/com/jeanaragao/workshopmongo/services/UserService.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.jeanaragao.workshopmongo.domain.User;
+import com.jeanaragao.workshopmongo.dto.UserDTO;
 import com.jeanaragao.workshopmongo.repository.UserRepository;
 import com.jeanaragao.workshopmongo.services.exceptions.ObjectNotFoundException;
 
@@ -26,4 +27,12 @@ public class UserService {
 		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
 	}
 	
+	public User insert(User obj) {
+		return userRepository.insert(obj);
+	}
+	
+	
+	public User fromDTO(UserDTO objDTO) {
+		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
+	}
 }
