17321b1ad5d64b8c867349eb4028233c57bbf81b
jeanaragaosouza <tuning.jean@hotmail.com>
2025-11-29 18:16:10 -0300

    Obtendo um post por id

---
+++ src/main/java/com/jeanaragao/workshopmongo/resources/PostResource.java
@@ -0,0 +1,31 @@
+package com.jeanaragao.workshopmongo.resources;
+
+import java.io.Serializable;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jeanaragao.workshopmongo.domain.Post;
+import com.jeanaragao.workshopmongo.services.PostService;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	@Autowired
+	private PostService postService;
+
+	
+	
+	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	public ResponseEntity<Post> findById(@PathVariable String id) {
+		Post obj = postService.findById(id);		
+		return ResponseEntity.ok().body(obj);
+	}
+
+}
