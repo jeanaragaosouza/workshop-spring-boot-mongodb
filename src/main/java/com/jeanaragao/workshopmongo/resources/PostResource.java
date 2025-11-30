f5a68e084e6fe950ac3313ea4341ccd63567decc
jeanaragaosouza <tuning.jean@hotmail.com>
2025-11-30 13:40:02 -0300

    Consulta simples com query methods

--- src/main/java/com/jeanaragao/workshopmongo/resources/PostResource.java
+++ src/main/java/com/jeanaragao/workshopmongo/resources/PostResource.java
@@ -1,15 +1,18 @@
 package com.jeanaragao.workshopmongo.resources;
 
 import java.io.Serializable;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jeanaragao.workshopmongo.domain.Post;
+import com.jeanaragao.workshopmongo.resources.util.URL;
 import com.jeanaragao.workshopmongo.services.PostService;
 
 @RestController
@@ -27,5 +30,13 @@ public class PostResource implements Serializable {
 		Post obj = postService.findById(id);		
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue = "")String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = postService.findByTitle(text);	
+		return ResponseEntity.ok().body(list);
+	}
+	
 
 }
